e161eccfaa833bfc24acae0230ef2ce907e8b5e4
dasha <dasha15042009@yandex.ru>
2022-08-21 17:44:49 +0300

    Ready 1 ex

--- First.cs
+++ First.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if(a>b)
+{
+    Console.Write("max=", a);
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max=");
+    Console.Write(b);
+}
